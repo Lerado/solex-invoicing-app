60352f7f726b5e9f4002310cdddcee7332ea4461
Darel Ngogang <darel.ngogang@gmail.com>
2024-03-04 10:09:01 +0100

    feat(splashscreen): add splashscreen to desktop app

---
+++ src/app/core/tauri/tauri.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, isDevMode } from '@angular/core';
+import { invoke } from '@tauri-apps/api';
+import { from, Observable, tap } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class TauriService {
+
+    /**
+     * Constructor
+     */
+    constructor() { }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    invoke(command: string): Observable<unknown> {
+
+        // Invoke command from Rust using Tauri
+        return from(
+            invoke(command)
+                .then(() => `command ${command} invoked successfully`)
+                .catch(error => `command ${command} invoked with error ${error.message}`)
+        )
+            .pipe(
+                tap((status) => {
+                    // Log in dev mode
+                    if (isDevMode()) {
+                        window.console.log(status);
+                    }
+                })
+            );
+    }
+}
