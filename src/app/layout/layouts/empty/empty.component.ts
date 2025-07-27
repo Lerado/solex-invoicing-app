1ebcaf8f93b9f7e6eae538f99c675c09a341ce42
Darel Ngogang <darel.ngogang@gmail.com>
2025-07-27 01:57:46 +0100

    fix(empty-layout): remove deprecated window commands buttons

--- src/app/layout/layouts/empty/empty.component.ts
+++ src/app/layout/layouts/empty/empty.component.ts
@@ -3,13 +3,12 @@ import { Component, signal, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FuseLoadingBarComponent } from '@fuse/components/loading-bar';
 import { platform } from '@tauri-apps/plugin-os';
-import { WindowCommandsComponent } from 'app/layout/common/window-commands/window-commands.component';
 
 @Component({
     selector: 'sia-empty-layout',
     templateUrl: './empty.component.html',
     encapsulation: ViewEncapsulation.None,
-    imports: [FuseLoadingBarComponent, WindowCommandsComponent, RouterOutlet]
+    imports: [FuseLoadingBarComponent, RouterOutlet]
 })
 export class EmptyLayoutComponent {
     platform = signal(platform() || 'browser').asReadonly();
